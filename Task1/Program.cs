6649901674ed426c6d127fc98ffd32d594827c0d
Vladimir <saab.team@yandex.ru>
2023-03-23 21:39:56 +0500

    added basic methods for creating initial array and printing arrays

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход значение, преообразует его в int
+string ReadData(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine()??"0";
+}
+
+// Создает массив из num элементов введеные пользователем
+string[] ArrayGen1D(int num)
+{
+    string[] array = new string[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i]  = ReadData($"Заполнитете элемент {i} из {num} текстом из клавиатуры");
+    }
+    return array;
+}
+
+// Печатает 1д массив
+void ArrayPrint1D(string[] arrayPrint)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arrayPrint.Length - 1; i++)
+    {
+        Console.Write(arrayPrint[i] + ", ");
+    }
+    Console.WriteLine(arrayPrint[arrayPrint.Length - 1] + " ]");
+
+}
