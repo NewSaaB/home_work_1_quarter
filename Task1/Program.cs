ec6ff96f623ed0788867b3932a10c48232a3175d
Vladimir <saab.team@yandex.ru>
2023-03-23 22:00:34 +0500

    the main code for the task is written

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,8 +1,15 @@
-﻿// Принимает на вход значение, преообразует его в int
-string ReadData(string msg)
+﻿// Принимает на вход строку
+string ReadDataStr(string msg)
 {
     Console.Write(msg);
     return Console.ReadLine() ?? "0";
+}
+
+// Принимает на вход значение и преобразует его в целое число
+int ReadDataInt(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
 }
 
 // Создает массив из num элементов введеные пользователем
@@ -11,7 +18,7 @@ string[] ArrayGen1D(int num)
     string[] array = new string[num];
     for (int i = 0; i < num; i++)
     {
-        array[i] = ReadData($"Заполнитете элемент {i} из {num} текстом из клавиатуры");
+        array[i] = ReadDataStr($"Заполнитете элемент {i+1} из {num} текстом из клавиатуры: ");
     }
     return array;
 }
@@ -46,4 +53,14 @@ string[] ArrayModNValue(string[] arrOld, int n)
 // Основной код программы
 // ==============================
 
-// 
+// 1. Пользователь создает массив
+int num = ReadDataInt("Какого размера вы хотите создать массив. Введите число: ");
+string[] arr = ArrayGen1D(num);
+
+// 2. Выводим пользователю его массив
+ArrayPrint1D(arr);
+
+// 3. Создаем новый массив на основе страого с учетом максимального количества символов, который скажет пользователь. Затем показываем его пользователю
+int limit = ReadDataInt("Сколько максимум должно быть символов в строке? Введите число: ");
+string[] arrLimit = ArrayModNValue(arr, limit);
+ArrayPrint1D(arrLimit);
